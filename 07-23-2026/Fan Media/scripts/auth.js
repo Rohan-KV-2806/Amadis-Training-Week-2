f8e9d50c7ec7a71c0a2c129b28a33c5f6ebf8726
Rohan-KV-2806 <rohankv28062004@gmail.com>
2026-07-23 13:00:16 +0530

    FAN Media - Routes implemented

--- 07-23-2026/Fan Media/scripts/auth.js
+++ 07-23-2026/Fan Media/scripts/auth.js
@@ -7,6 +7,7 @@ function signup(){
     };
     localStorage.setItem(user_name, JSON.stringify(accounts));
     alert("account" + user_name + "is successfully created");
+    window.location.href = "login.html";
 }
 
 function login(){
@@ -14,7 +15,9 @@ function login(){
     let User_password = document.getElementById("pass").value;
     let user = JSON.parse(localStorage.getItem(User_name));
     if(User_password == user.password){
+        let name = user.name;
         alert("login success");
+        window.location.href = `index.html?loggedin_user=${encodeURIComponent(name)}`;
     }else{
         alert("Login unsuccessful");
     }
